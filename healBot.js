9bdf49dcad237663dfb878b53a02a9df837a2114
EnterElysium <enterelysiumyt@gmail.com>
2022-06-20 10:21:28 +0100

    finish testing

--- healBot.js
+++ healBot.js
@@ -2,7 +2,7 @@ const healBot = (function() {
 
 	const scriptIndex = {"name":"healBot","version":"v0.01",};
 
-	// format text message feedback to look pretty right?
+	// format text message feedback to look pretty
 
 	on("chat:message", function(msg) {
 
